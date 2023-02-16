024dd55b63d32fec294b741c66d5599409756a84
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-02-16 21:42:58 +0530

    [Sangmeshwar] Add: Editing Method Added To Edit the Existing Contact

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -8,9 +8,28 @@ namespace AddressBookSystem
         {
             Console.WriteLine("---Welcome To Address Book Management System---");
             AddressBook addressbook = new AddressBook();
-            addressbook.CreateContact();
-            addressbook.Disply();
-            Console.ReadLine();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select 1: For Creating Contact"+ "\nSelect 2: Edit Contact" + "\nSelect 3: Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addressbook.CreateContact();
+                        addressbook.Display();
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Edit Contact : ");
+                        addressbook.EditContact("Sangmeshwar");
+                        addressbook.Display();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
