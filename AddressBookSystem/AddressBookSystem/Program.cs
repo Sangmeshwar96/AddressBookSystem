3ea72cf54ef33051e6e0b7adc920ff09d77bc0bc
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-02-17 22:20:16 +0530

    [Sangmeshwar] Add: Adding Multiple Contacts To Address Book

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -11,7 +11,7 @@ namespace AddressBookSystem
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nSelect 1: For Creating Contact"+ "\nSelect 2: Edit Contact"+ "\nSelect 3: Delete Contact"+ "\nSelect 4: Display Contact" + "\nSelect 5: Exit");
+                Console.WriteLine("\nSelect 1:To Create Contact"+ "\nSelect 2:To Display Contact"+"\nSelect 3:To Edit Contact"+"\nSelect 4:To Delete Contact"+"\nSelect 5:To Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -20,17 +20,17 @@ namespace AddressBookSystem
                         //addressbook.Display();
                         break;
                     case 2:
+                        addressbook.Display();
+                        Console.ReadLine();
+                        break;
+                    case 3:
                         Console.WriteLine("Edit Contact : ");
                         addressbook.EditContact("Sangmeshwar");
                         //addressbook.Display();
                         break;
-                    case 3:
+                    case 4:
                         addressbook.DeleteContact();
                         //addressbook.Display();
-                        break;
-                    case 4:
-                        addressbook.Display();
-                        Console.ReadLine();
                         break;
                     case 5:
                         Console.WriteLine("\nThanking You !!!\nVisit Again !!!");
